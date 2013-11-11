dcbb01d44132c45b05e0f572758d427b9d07ea21
Anne Sturdivant <anne@opensourcery.com>
2013-11-11 14:03:55 -0800

    theme updates: header and footer styles; updates to tpls; updated config.rb & theme info

--- plugins/layouts/variolite/variolite.tpl.php
+++ plugins/layouts/variolite/variolite.tpl.php
@@ -18,23 +18,33 @@
 <div class="panel-display variolite clearfix <?php if (!empty($class)): print $class; endif; ?>" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
 
   <div class="variolite-sidebar panel-panel">
+    <div class="variolite-sidebar-inner panel-panel-inner">
       <?php print $content['sidebar']; ?>
+    </div><!-- /.variolite-sidebar-inner -->
   </div><!-- /.variolite-sidebar -->
 
   <div class="variolite-content-container">
-    <div class="variolite-content-header clearfix panel-panel">
-        <?php print $content['content_header']; ?>
-    </div><!-- /.variolite-content-container -->
+    <?php if ($content['content_header']): ?>
+      <div class="variolite-content-header clearfix panel-panel">
+          <?php print $content['content_header']; ?>
+      </div><!-- /.variolite-content-container -->
+    <?php endif; ?>
 
-    <div class="variolite-content-container-column-container clearfix">
+    <div class="variolite-content-container-column-container clearfix <?php if ($content['content_secondary_column']): print 'with-secondary-column'; endif; ?>">
 
       <div class="variolite-content-main-column variolite-column panel-panel">
+        <div class="variolite-content-main-column-inner variolite-column-inner panel-panel-inner">
           <?php print $content['content_main_column']; ?>
+        </div><!-- /.variolite-content-main-column-inner -->
       </div><!-- /.variolite-content-main-column -->
 
-      <div class="variolite-content-secondary-column variolite-column panel-panel">
-          <?php print $content['content_secondary_column']; ?>
-      </div><!-- /.variolite-content-main-column -->
+      <?php if ($content['content_secondary_column']): ?>
+        <div class="variolite-content-secondary-column variolite-column panel-panel">
+          <div class="variolite-content-secondary-column-inner variolite-column-inner panel-panel-inner">
+            <?php print $content['content_secondary_column']; ?>
+          </div><!-- /.variolite-content-main-column-inner -->
+        </div><!-- /.variolite-content-main-column -->
+      <?php endif; ?>
 
     </div><!-- /.variolite-content-container-column-container -->
 
