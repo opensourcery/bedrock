00d6db31c352373508704c7b4890485bae8b8f6b
Anne Sturdivant <anne@opensourcery.com>
2013-11-18 17:32:41 -0800

    updated variolite markup to html5 elements

--- plugins/layouts/variolite/variolite.tpl.php
+++ plugins/layouts/variolite/variolite.tpl.php
@@ -17,13 +17,13 @@
 
 <div class="panel-display variolite clearfix <?php if (!empty($class)): print $class; endif; ?>" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
 
-  <div class="variolite-sidebar panel-panel">
+  <section class="variolite-sidebar panel-panel">
     <div class="variolite-sidebar-inner panel-panel-inner">
       <?php print $content['sidebar']; ?>
     </div><!-- /.variolite-sidebar-inner -->
-  </div><!-- /.variolite-sidebar -->
+  </section><!-- /.variolite-sidebar -->
 
-  <div class="variolite-content-container">
+  <section class="variolite-content-container">
     <?php if ($content['content_header']): ?>
       <div class="variolite-content-header clearfix panel-panel">
           <?php print $content['content_header']; ?>
@@ -32,22 +32,22 @@
 
     <div class="variolite-content-container-column-container clearfix <?php if ($content['content_secondary_column']): print 'with-secondary-column'; endif; ?>">
 
-      <div class="variolite-content-main-column variolite-column panel-panel">
+      <article class="variolite-content-main-column variolite-column panel-panel">
         <div class="variolite-content-main-column-inner variolite-column-inner panel-panel-inner">
           <?php print $content['content_main_column']; ?>
         </div><!-- /.variolite-content-main-column-inner -->
-      </div><!-- /.variolite-content-main-column -->
+      </article><!-- /.variolite-content-main-column -->
 
       <?php if ($content['content_secondary_column']): ?>
-        <div class="variolite-content-secondary-column variolite-column panel-panel">
+        <aside class="variolite-content-secondary-column variolite-column panel-panel">
           <div class="variolite-content-secondary-column-inner variolite-column-inner panel-panel-inner">
             <?php print $content['content_secondary_column']; ?>
           </div><!-- /.variolite-content-main-column-inner -->
-        </div><!-- /.variolite-content-main-column -->
+        </aside><!-- /.variolite-content-main-column -->
       <?php endif; ?>
 
     </div><!-- /.variolite-content-container-column-container -->
 
-  </div><!-- /.variolite-content-container -->
+  </section><!-- /.variolite-content-container -->
 
 </div><!-- /.variolite -->
