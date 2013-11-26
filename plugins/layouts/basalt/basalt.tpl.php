08f987e1b37ee9575a49c2ed25c886b41f799284
Anne Sturdivant <anne@opensourcery.com>
2013-11-26 11:15:51 -0800

    basalt tpl cleanup; comment update

--- plugins/layouts/basalt/basalt.tpl.php
+++ plugins/layouts/basalt/basalt.tpl.php
@@ -8,20 +8,21 @@
  * - $css_id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
  *   panel of the layout. This layout supports the following sections:
- *   -- Content Header
- *   -- Column Left
- *   -- Column Middle
- *   -- Column Right
+ *   -- Content Header ['content_header']
+ *   -- Column Left ['column_left']
+ *   -- Column Middle ['column_middle']
+ *   -- Column Right ['column_right']
  */
 ?>
 
 <div class="panel-display basalt clearfix <?php if (!empty($class)): print $class; endif; ?>" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
 
   <div class="basalt-content-container">
+
     <?php if ($content['content_header']): ?>
       <div class="basalt-content-header clearfix panel-panel">
           <?php print $content['content_header']; ?>
-      </div><!-- /.basalt-content-container -->
+      </div><!-- /.basalt-content-header -->
     <?php endif; ?>
 
     <div class="basalt-column-container clearfix">
@@ -44,7 +45,7 @@
         </div><!-- /.basalt-column-right-inner -->
       </div><!-- /.basalt-column-right -->
 
-    </div><!-- /.basalt-content-container-column-container -->
+    </div><!-- /.basalt-column-container -->
 
   </div><!-- /.basalt-content-container -->
 
